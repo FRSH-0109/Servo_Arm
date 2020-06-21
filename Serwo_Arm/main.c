9aecdc00a6307af873def29b7f00c2d3a8214a19
Kamil <kamikosnik@gmail.com>
2020-06-21 23:58:31 +0200

    uartTest

--- Serwo_Arm/main.c
+++ Serwo_Arm/main.c
@@ -1,18 +1,39 @@
-/*
- * Serwo_Arm.c
- *
- * Created: 21.06.2020 23:30:07
- * Author : kamil
- */ 
-
 #include <avr/io.h>
 
+#define FOSC 8000000	// Clock Speed
+#define BAUD 9600
+#define MYUBRR FOSC/16/BAUD-1
 
+void USART_Init(unsigned int ubrr);
+void USART_Transmit(unsigned char data);
+
+uint8_t value = 4;
 int main(void)
 {
-    /* Replace with your application code */
+    USART_Init(MYUBRR);
+	USART_Transmit(value);
     while (1) 
     {
     }
+}
+
+void USART_Init(unsigned int ubrr)
+{
+	/*Set baud rate */
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	//Enable receiver and transmitter 
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	/* Set frame format: 8data, 2stop bit */
+	UCSR0C = (1<<USBS0)|(3<<UCSZ00);
+	}
+	
+	void USART_Transmit(unsigned char data)
+{
+/* Wait for empty transmit buffer */
+while (!(UCSR0A & (1<<UDRE0)))
+;
+/* Put data into buffer, sends the data */
+UDR0 = data;
 }
 
