00fefd0bc1ae7b3c2ec81cca28d32fdca7a5d645
Kamil <kamikosnik@gmail.com>
2020-06-21 23:36:29 +0200

    1

---
+++ Serwo_Arm/main.c
@@ -0,0 +1,18 @@
+/*
+ * Serwo_Arm.c
+ *
+ * Created: 21.06.2020 23:30:07
+ * Author : kamil
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
